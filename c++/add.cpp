8b1fc952e10242a74fa5f279d28e19b88a979148
Ubuntu <rufino@Administration-Machine.kkhsn03u4peezhgmffehedrrtb.gx.internal.cloudapp.net>
2020-04-04 02:27:39 +0000

    created a c++ code

---
+++ c++/add.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    cout << "This program takes two integers from the user, adds the integers, and outputs the result" << endl;
+    
+    cout << "Enter the first integer: ";
+    cin >> a;
+    
+    cout << "Enter the second integer: ";
+    cin >> b;
+    
+    cout << "The result of adding " << a << " and " << b << " is: " << a + b << endl;
+    return 0;
+}
